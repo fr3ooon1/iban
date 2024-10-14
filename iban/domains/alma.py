391f6961b90f1a99ab618187f6c2fce9e644f511
Eslam Ragab <eslamragab798@gmail.com>
2024-10-14 19:04:26 +0200

    Your commit message

--- iban/domains/alma.py
+++ iban/domains/alma.py
@@ -51,6 +51,54 @@ data = {
 					"insert_after": "column_section102",
 					"label": _("Notes"),
 				},
+        ],
+                "Sales Invoice":[
+        {
+					"fieldname": "section_section101",
+					"fieldtype": "Section Break",
+					"insert_after": "set_target_warehouse",
+					"label": _("Cars Detials"),
+				},
+				{
+					"fieldname": "plate_number",
+					"fieldtype": "Data",
+					"insert_after": "section_section101",
+					"label": _("Plate Number"),
+				},
+        {
+					"fieldname": "car_color",
+					"fieldtype": "Data",
+					"insert_after": "plate_number",
+					"label": _("Car Color"),
+				},
+        {
+					"fieldname": "column_break101",
+					"fieldtype": "Column Break",
+					"insert_after": "car_color",
+				},
+        {
+					"fieldname": "car_type",
+					"fieldtype": "Data",
+					"insert_after": "column_break101",
+					"label": _("Car Type"),
+				},
+        {
+					"fieldname": "installed_order",
+					"fieldtype": "Data",
+					"insert_after": "car_type",
+					"label": _("Installed Order"),
+				},
+        {
+					"fieldname": "column_section102",
+					"fieldtype": "Column Break",
+					"insert_after": "installed_order",
+				},
+        {
+					"fieldname": "notes",
+					"fieldtype": "Small Text",
+					"insert_after": "column_section102",
+					"label": _("Notes"),
+				},
         ]
 
 		},
